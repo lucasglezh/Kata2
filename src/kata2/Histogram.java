32b6c54dc8c27d77fd9c898671920f272d391932
usuario <usuario@LUCAS>
2020-10-21 16:53:25 +0100

    Cuarta versión Kata2

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,22 +3,21 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
-    private final int[] data;
+public class Histogram<T> {
+    private final T[] data;
     
-    public Histogram (int[] data){
+    public Histogram (T[] data){
         this.data = data;
     }
     
-    public int[] getData(){
+    public T[] getData(){
         return data;
     }
     
-    Map<Integer,Integer> getHistogram(){
-        Map<Integer,Integer> histogram = new HashMap<Integer,Integer>();
-        for (int i : data) {
-           histogram.put(data[i], histogram.containsKey(data[i]) 
-                   ? histogram.get(data[i])+1:1); 
+    Map<T,Integer> getHistogram(){
+        Map<T,Integer> histogram = new HashMap<T,Integer>();
+        for (T i : data) {
+           histogram.put(i, histogram.containsKey(i)? histogram.get(i)+1:1); 
         }
         return histogram;
     }
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[] data = new int[] {0,1,2,3,4,5,6,7,8,9,0,3,6,9,0};
+        Integer[] data = {0,1,2,3,4,5,6,7,8,9,0,3,6,9,0};
         Histogram histo = new Histogram(data);
         Map<Integer,Integer> histogr = histo.getHistogram();
         for (Map.Entry<Integer, Integer> entry : histogr.entrySet()) {
